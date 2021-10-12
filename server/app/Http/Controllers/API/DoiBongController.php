6da393768e290729de41ddbaa0fad1b0a25b6238
Huy Nguyen <nguyenquochuyhbt@gmail.com>
2021-10-12 17:03:35 +0700

    test CRUD

--- server/app/Http/Controllers/API/DoiBongController.php
+++ server/app/Http/Controllers/API/DoiBongController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
-use App\Models\DoiBong;
 use Illuminate\Http\Request;
+use App\Models\DoiBong;
+use App\Http\Requests\DoiBongStore;
+use App\Http\Requests\DoiBongUpdate;
 
 class DoiBongController extends Controller
 {
@@ -12,8 +14,22 @@ class DoiBongController extends Controller
         return DoiBong::all();
     }
 
-    public function store(Request $request){
-        $data=$request->all();
+    public function store(DoiBongStore $request){
+        $data=$request->validated();
         return DoiBong::create($data);
+    }
+
+    public function show(DoiBong $doiBong){
+        return $doiBong;
+    }
+
+    public function update(DoiBongUpdate $request, DoiBong $doiBong)
+    {
+        $find = DoiBong::find($doiBong);
+        if(is_null($find)){
+            return response()->json('Record Not Found', 404);
+        }
+        $data=$request->validated();
+        return $doiBong->update($data);
     }
 }
