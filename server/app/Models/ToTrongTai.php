c366395dd251a4110a6afcddd9ed0f8795dedff2
= <=>
2021-11-28 23:32:05 +0700

    update totrongtai dat

--- server/app/Models/ToTrongTai.php
+++ server/app/Models/ToTrongTai.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class ToTrongTai extends Model
 {
     use HasFactory;
-}
+
+    protected $table = 'totrongtai';
+    protected $primaryKey = 'idToTT';
+
+    protected $fillable =[
+        'idTT',
+        'created_at',
+        'updated_at'
+    ];
+
+    public function To_trong_tai(){
+        return $this->belongsTo(TrongTai::class, 'idToTT');
+
+    }}
