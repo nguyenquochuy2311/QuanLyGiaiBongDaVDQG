a964239635850ac266bca49ad21ab9508b6e3be2
Hunga9k50doker <19520101@gm.uit.edu.vn>
2021-12-07 23:20:05 +0700

    validate crud

---
+++ .history/client/src/views/App_20211207225023.js
@@ -0,0 +1,179 @@
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.scss";
+import "../styles/_variables.scss";
+import "boxicons";
+//import components
+import Nav from "../containers/Navigation/nav";
+import Footer from "../containers/footer/footer";
+import ScrollToTop from "../components/btnBackTop/btn";
+//import views
+import Home from "../views/home/home";
+import RankTeam from '../components/rankTables/rankTeams';
+import RankPlayer from '../components/rankTables/rankPlayers';
+import MatchDetail from './matchs/matchDetails';
+import MatchResult from './matchs/matchResult';
+import MatchCalendar from "./matchs/matchCalendar";
+import Referees from '../components/rankTables/referees';
+
+import Regulation from "../views/regulation/regulation";
+import Contact from "../views/contact/contact";
+import Team from "../views/team/team";
+import TeamDetail from "../views/team/clubDetail";
+
+import Login from "../views/accounts/login";
+import Register from "../views/accounts/register";
+
+import Club from '../views/admin/club/club';
+import AddClub from '../views/admin/club/addClub';
+import EditClub from '../views/admin/club/editclub';
+
+import Match from '../views/admin/match/match';
+import AddMatch from '../views/admin/match/addMatch';
+// import EditMatch from '../views/admin/match/editMatch';
+
+
+
+import AdminRegulation from '../views/admin/regulation/regulation';
+import EditDiemSo from '../views/admin/regulation/editRegulation';
+import EditBanThang from '../views/admin/regulation/editTypeGoal';
+
+
+import Player from '../views/admin/player/player';
+import AddPlayer from '../views/admin/player/addPlayer';
+import EditPlayer from '../views/admin/player/editPlayer';
+
+
+
+import ResultMatch from '../views/admin/match/resultMatch';
+import AddResultMatch from '../views/admin/match/addResult';
+// import EditResultMatch from '../views/admin/match/editResult';
+
+import AdminAccount from '../views/admin/user/user';
+// import AddAccount from '../views/admin/user/addUser';
+// import EditAccount from '../views/admin/user/editUser';
+
+
+import NotFound from "../views/404NotFound/find";
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Nav />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route  exact path="/doi-bong">
+            <Team />
+          </Route>
+          <Route  exact path="/doi-bong/:slug">
+            <TeamDetail />
+          </Route>
+          <Route  exact path="/quy-dinh">
+            <Regulation />
+          </Route>
+          <Route exact path="/giai-dau/bxh-doi-bong">
+            <RankTeam />
+          </Route>
+         
+
+          <Route  exact path="/giai-dau/bxh-cau-thu">
+            <RankPlayer />
+          </Route>
+          <Route  exact path="/giai-dau/cac-tran-dau">
+            <MatchCalendar />
+          </Route>
+          <Route  exact path="/giai-dau/ket-qua-tran-dau">
+            <MatchResult  />
+          </Route>
+          <Route  exact path="/giai-dau/ket-qua-tran-dau/:slug">
+            <MatchDetail />
+          </Route>
+
+          <Route  exact path="/giai-dau/trong-tai">
+            <Referees />
+          </Route>
+          <Route  exact path="/lien-he">
+            <Contact />
+          </Route>
+          <Route  exact path="/dang-nhap">
+            <Login/>
+          </Route>
+          <Route  exact path="/dang-ky">
+            <Register />
+          </Route>
+
+          {/* ===admin============ */}
+          
+          <Route  exact  path="/admin">
+            <Club />
+          </Route>
+          <Route  exact path="/admin/club/add">
+            <AddClub />
+          </Route>
+          <Route  exact path="/admin/club/edit/:id">
+            <EditClub />
+          </Route>
+          <Route  exact path="/admin/lich-dau">
+            <Match />
+          </Route>
+          <Route  exact path="/admin/lich-dau/add">
+            <AddMatch />
+          </Route>
+          {/* <Route  exact path="/admin/lich-dau/edit/:id">
+            <EditMatch />
+          </Route> */}
+
+
+          <Route  exact path="/admin/ket-qua-tran-dau">
+            <ResultMatch />
+          </Route>
+          <Route  exact path="/admin/ket-qua-tran-dau/add">
+            <AddResultMatch />
+          </Route>
+          <Route  exact path="/admin/ket-qua-tran-dau/add">
+            <AddResultMatch />
+          </Route>
+
+
+          <Route  exact path="/admin/cau-thu">
+            <Player />
+          </Route>
+          <Route  exact path="/admin/cau-thu/add">
+            <AddPlayer />
+          </Route>
+          <Route  exact path="/admin/cau-thu/edit/:id">
+            <EditPlayer />
+          </Route>
+
+
+
+          <Route  exact path="/admin/quy-dinh">
+            <AdminRegulation   />
+          </Route>
+          <Route  exact path="/admin/quy-dinh/diem-so/edit/:id">
+            <EditDiemSo   />
+          </Route>
+          <Route  exact path="/admin/quy-dinh/loai-ban-thang/edit/:id">
+            <EditBanThang  />
+          </Route>
+
+          <Route  exact path="/admin/tai-khoan">
+            <AdminAccount   />
+          </Route>
+          <Route  exact path="*">
+            <NotFound   />
+          </Route>
+
+        </Switch>
+        <ScrollToTop />
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ .history/server/app/Http/Controllers/API/CauThuController_20211207223841.php
@@ -0,0 +1,196 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreCauThuRequest;
+use App\Http\Requests\UpdateCauThuRequest;
+use Illuminate\Support\Facades\DB;
+use App\Models\CauThu;
+
+class CauThuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // $cau_thu = DB::table('cauthu')
+        //     ->join('clb', 'cauthu.idCLB', '=', 'clb.idCLB')
+        //     ->select('cauthu.*', 'clb.TenCLB')
+        //     ->get();
+        // $cau_thu = CauThu::with('clb')->get();
+        $cau_thu = CauThu::orderBy('idCT', 'DESC')->with('clb')->get();
+        return response($cau_thu);
+       
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return response([
+            'status' => 201,
+            'message' => 'Thêm cầu thủ thành công'
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCauThuRequest $request)
+    {
+        $data = DB::table('cauthu')
+                ->where('idCLB',$request->idCLB)
+                ->where('SoAo',$request->SoAo)
+                ->select()
+                ->get();
+                // return response(count($data));
+        $var = count($data);
+        if($var != 0 ){
+            return response([
+                'status' => 204,
+                'message' => "Không thế thêm cầu thủ. Vì đã có cầu thủ mang áo số $request->SoAo"
+            ]);
+        }
+        else{
+            CauThu::create($request->all());  
+            return response([
+                'status' => 200,
+                'message' => "Thêm thành công"
+            ]);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $data = CauThu::find($id);
+        if(empty($data)){
+            return response([
+                'status' => 404,
+                'message' => 'Không tìm thấy'
+            ]);
+        }
+        // $cau_thu = DB::table('cau_thus')
+        //     ->join('doi_bongs', 'cau_thus.doi_bong_id', '=', 'doi_bongs.id')
+        //     ->where('cau_thus.id', '=', $id)
+        //     ->select('cau_thus.*', 'doi_bongs.ten_doi_bong')
+        //     ->get(); 
+        return response($data);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $data = CauThu::find($id);
+        if(empty($data)){
+            return response([
+                'status' => 404,
+                'message' => 'Không tìm thấy'
+            ]);
+        }
+        return response($data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $Cauthu = CauThu::find($id);
+        if(empty($Cauthu)){
+            return response([
+                'status' => 404,
+                'message' => 'Không tìm thấy'
+            ]);
+        }else{
+            $data = DB::table('cauthu')
+            ->where('idCLB',$request->idCLB)
+            ->where('SoAo',$request->SoAo)
+            ->where('AnhDaiDien',$request->AnhDaiDien)
+            ->where('SoAo',$request->SoAo)
+
+            ->whereNotIn('idCT',[$id])
+            ->select()
+            ->get();
+            // return response(count($data));
+            $var = count($data);
+            if($var > 0 ){
+                return response([
+                    'status' => 204,
+                    'message' => "Không thể cập nhập cầu thủ. Vì đã có cầu thủ mang áo số $request->SoAo"
+                ]);
+            }
+            else{
+                $Cauthu->update($request->all());
+                return response([
+                    'status' => 200,
+                    'message' => 'Cập nhật thành công',
+                    "new_data" => $Cauthu
+                ]);
+            }
+        }
+        
+       
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $cauthu = CauThu::find($id);
+        // $data = count($cauthu);
+         if(empty($cauthu)){
+            return response([
+                'status' => 404,
+                'message' => 'Không tìm thấy cầu thủ cần xoá'
+            ]);
+        }else{
+            $cauthu->delete();
+            return response([
+                'status' => 200,
+                'message' => 'Xóa thành công'
+            ]);
+        }
+    }
+
+    public function search($tenCT)
+    {
+        $result = CauThu::where('TenCT', 'like', '%'.$tenCT.'%')->get();
+        if(count($result)){
+            return $result;
+        }
+        return response([
+            'status' => 404,
+            'message' => 'Không tìm thấy'
+        ]);
+    }
+}
